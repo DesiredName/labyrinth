8299ea032cc110e855909fd20d5585aaf44819fc
DesiredName <wc3-enter@yandex.ru>
2025-11-01 22:05:59 +0100

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,6 @@ module.exports = {
   ignorePatterns: ["dist", "node_modules"],
   rules: {
     "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
   }
 }
